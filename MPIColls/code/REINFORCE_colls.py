454a4d4c3aadb8d52a5e47449ce14528d4ffc022
sergio <smoreno@unex.es>
2020-09-09 13:56:00 +0200

    std_error shaded

--- MPIColls/code/REINFORCE_colls.py
+++ MPIColls/code/REINFORCE_colls.py
@@ -517,27 +517,32 @@ X_AXIS = 100
 
 j = j.reshape((X_AXIS, -1))
 
-j1 = j.max(axis=1)
-j2 = j.min(axis=1)
-j = j.mean(axis=1)
+j_max = j.max(axis=1)
+j_min = j.min(axis=1)
+j_mean = j.mean(axis=1)
 
 t = t.reshape((X_AXIS, -1))
 t = t.mean(axis=1)
 
-max_val = []
-min_val = []
+arr_mean = []
+for i in j_mean:
+  arr_mean.append(i.item())
 
-for i,k in zip(j1,j2):
-  max_val.append(i.item())
-  min_val.append(k.item())  
+
+j_ndarr = np.empty(j.shape, dtype=float)
+for i in range(0, j.shape[0]-1):
+  for k in range(0, j.shape[1]-1):
+    j_ndarr[i][k] = j[i][k].item()
+
+j_std = np.std(j_ndarr, axis=1)
 
 
 plt.figure(figsize=(12,8))
 plt.axis('on')
 
-plt.plot(np.arange(0, X_AXIS, 1), j, color='blue', marker='.')
+plt.plot(np.arange(0, X_AXIS, 1), j_mean, color='blue', marker='.')
 
-plt.fill_between(np.arange(0, X_AXIS, 1), max_val, min_val, color='blue', alpha=0.2)
+plt.fill_between(np.arange(0, X_AXIS, 1), arr_mean - j_std, arr_mean + j_std, color='blue', alpha=0.2)
 
 plt.title("Cost function per Episode")
 plt.xlabel('# Episode')
