5b9aebd9325ab18115a8d9581b228adc478e3be2
Juan A. Rico <jarico@Ambroz.home>
2020-09-09 13:15:46 +0200

    Cleaning code ...

--- MPIColls/code/gym-mpicolls/gym_mpicolls/envs/mpicolls_env.py
+++ MPIColls/code/gym-mpicolls/gym_mpicolls/envs/mpicolls_env.py
@@ -150,80 +150,27 @@ def get_reward(s, M, P):
 
 class MPICollsEnv(gym.Env):
 	metadata = {'render.modes': ['human']}
-		
+	
+	
 	def __init__(self, params):
 		super(MPICollsEnv, self).__init__()
 		
-		print(params)
-	
+		self.params = params
+		print(self.params)
+
 		# ACTION space
-		self.action_space = Edge(P=params["P"])
+		self.action_space = Edge(P=self.params["P"])
 	
 		# STATES space
-		self.observation_space = Matrix(P=params["P"])
-	
-		# Parameters for invocation:
-		self.params = params
+		self.observation_space = Matrix(P=self.params["P"])
 		
 		self.state = None
-		self.P = params["P"]
+		self.P = self.params["P"]
 		
-		self.M = np.arange(0, params["P"], 1)
+		self.M = np.arange(0, self.params["M"], 1)
 	
 		self.trajectory = 0
-
-
-	# This function returns a reward that favors Linear Trees.
-	def step_ (self, action):
-		
-		src, dst = action
-		
-		# Multicore machine. Hop is a network communication.
-		hop = 0
-		if self.M[src] != self.M[dst]:
-			hop = 1
-		
-		current_depth = np.max(np.max(self.state, axis=0))
-
-		# Stage: last stage of sending/receiving of a process + 1:
-		stage = np.maximum( np.amax(self.state[src,:]), np.amax(self.state[:,src]) ) + 1
 	
-		valid = True
-		reward = 0
-		done  = False
-
-		# Case 1: A process sends w/o previously receiving:
-		recv = np.max(self.state, axis=0)  # Last stage of reception of each process
-		if (recv[src] <= 0) or (recv[src] >= stage):
-			valid = False
-
-		# Case 2: A process receives an already received message:
-		#         axis=0 is by COLUMNS
-		if (recv[dst] > 0):
-			valid = False
-
-		# Case 3: send self:
-		if (src == dst):
-			valid = False
-	
-	
-		if (not valid):
-			reward = -1
-			# reward = 0.0
-		else: # if (valid == True):
-			self.state[src,dst] = stage
-			reward = 0.0
-			
-			# done = True if all processes received (columns have a value != -1)
-			if np.min(np.max(self.state, axis=0)) > 0:
-				done = True
-				reward = self.P * get_reward(self.state, self.M, self.P)
-				# reward = -1 * get_reward_tlop (self.state, self.M, self.params)
-				# reward = -1 * get_reward_mpi (self.state, self.M, self.params, self.trajectory)
-				# print("REWARDS: ", reward, flush=True)
-				self.trajectory += 1
-	
-		return np.array(self.state), reward, done, {}
 	
 	
 	# This function returns a reward that favors Linear Trees.
@@ -251,7 +198,6 @@ class MPICollsEnv(gym.Env):
 		if (src == dst):
 			valid = False
 		
-		
 		if (not valid):
 			reward = -1
 			# reward = 0
@@ -276,8 +222,7 @@ class MPICollsEnv(gym.Env):
 
 		return np.array(self.state), reward, done, {}
 
-	
-	
+
 		
 	def reset(self):
 
@@ -290,10 +235,12 @@ class MPICollsEnv(gym.Env):
 		self.state[root, root] = 1
 		
 		return np.copy(self.state)
-		
+
+	
 		
 	def render(self, mode='human', close=False):
 		print(self.state)
+	
 	
 	
 	def close(self):
