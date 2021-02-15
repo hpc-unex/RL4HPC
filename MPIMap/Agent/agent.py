Reward value sign problem fixed.

MPIMap/Agent/agent.py
@@ -127,7 +127,7 @@ class Agent(object):
 		action_probs = self.policy(state_tensor)
 		action_probs = action_probs.view(self.P, -1)
 
-		
+
 		# Using "masking" of action probabilities
 		# Set action_probs to -torch.inf for nodes full
 		capacity = self.capacity.clone().detach()
@@ -201,15 +201,16 @@ class Agent(object):
 	def learn (self):
 
 		# Compute discounted rewards (to Tensor):
+		# print("[REINFORCE] rewards: ", self.saved_rewards)
 		discounted_reward = self.get_return(self.saved_rewards).to(self.device)
-		# print("[REINFORCE] discounted_reward: ", discounted_reward)
+		# print("[REINFORCE] discounted_reward: ", discounted_reward)
 
 		# Compute log_probs:
 		logprob_tensor = torch.stack(self.saved_logprobs).to(self.device)
-		# print("[REINFORCE] logprob: ", logprob_tensor)
+		# print("[REINFORCE] logprob: ", logprob_tensor)
 
 		# Compute loss:
-		loss = logprob_tensor * discounted_reward
+		loss = -logprob_tensor * discounted_reward
 		# print("[REINFORCE] v. loss: ", loss)
 		loss = torch.sum(loss)
 		# print("[REINFORCE] loss: ", loss)

MPIMap/Env/self/self_reward.py
@@ -31,17 +31,17 @@ def get_reward(state, actions, params):
 	D = torch.zeros((P, P)).detach().to(device)
 	for i in range(0, P):
 		for j in range(0, P):
-			if i == j:
-				value = 10
-			elif actions[i] == actions[j]:
-				value = 1
-			else:
-				value = 2
+			if i == j:                         ## Same processor
+				value = 1 # 10
+			elif actions[i] == actions[j]:     ## Same node
+				value = 1 # 2
+			else:                              ## Different node
+				value = 2 # 1
 			D[i,j] = value
 
 	r = torch.sum(state * D).item() # - Baseline.get()
 	# print("Coste: ", r)
-	r = r / 100000
+	r = -r / 100000
 
 	valid = True
 